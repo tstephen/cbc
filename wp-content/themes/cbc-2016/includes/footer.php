19d0c9d2a863506783e2f1eb234647bfb211c3a2
Tim Stephenson <tim@knowprocess.com>
2017-10-29 22:00:58 +0000

    embed GA code directly instead of plugin

--- wp-content/themes/cbc-2016/includes/footer.php
+++ wp-content/themes/cbc-2016/includes/footer.php
@@ -14,5 +14,14 @@
 </nav>
 
 <?php wp_footer(); ?>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40400507-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-40400507-2');
+</script>
 </body>
 </html>
