121069843926217e5aba77006571b435437f8f17
Tim Stephenson <tim@knowprocess.com>
2017-10-29 21:59:35 +0000

    formatting around catpions and sermons inc. response form

--- wp-content/themes/cbc-2016/js/bst.js
+++ wp-content/themes/cbc-2016/js/bst.js
@@ -13,13 +13,27 @@
 		$('input[type=submit]').addClass('btn btn-primary');
 		
 		// Sermon contact form
-		jQuery('.sermon-contact:first').empty().append('<span class="dashicons dashicons-testimonial"></span><a class="sermon-contact-button" href="#">Ask a question or make a comment</a>');
-		jQuery('.sermon-contact:first .sermon-contact-button')
+		jQuery('.sermon-contact:eq(0), .sermon-contact:eq(1), .sermon-contact:eq(2)').empty().append('<span class="dashicons dashicons-testimonial"></span><a class="sermon-contact-button" href="#">Ask a question or make a comment</a>');
+		jQuery('.sermon-contact:eq(0) .sermon-contact-button')
 			.click(function() {
 				jQuery('html, body').animate({
-					scrollTop: jQuery(".sermon-contact:first").offset().top
+					scrollTop: jQuery(".sermon-contact:eq(0)").offset().top
 				}, 2000);
-				jQuery('.sermon-form:first').slideDown();
+				jQuery('.sermon-form:eq(0)').slideDown();
+			});
+		jQuery('.sermon-contact:eq(1) .sermon-contact-button')
+			.click(function() {
+				jQuery('html, body').animate({
+					scrollTop: jQuery(".sermon-contact:eq(1)").offset().top
+				}, 2000);
+				jQuery('.sermon-form:eq(1)').slideDown();
+			});
+		jQuery('.sermon-contact:eq(2) .sermon-contact-button')
+			.click(function() {
+				jQuery('html, body').animate({
+					scrollTop: jQuery(".sermon-contact:eq(2)").offset().top
+				}, 2000);
+				jQuery('.sermon-form:eq(2)').slideDown();
 			});
 
 	});
