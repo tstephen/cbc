8b7145370552c9593ce1cf3d1bfc88cb3252bf91
Tim Stephenson <tim@knowprocess.com>
2018-10-30 17:03:14 +0000

    upgrade sermon-manager-for-wordpress gutenberg featured-image-from-url contact-form-7

--- wp-content/plugins/featured-image-from-url/featured-image-from-url.php
+++ wp-content/plugins/featured-image-from-url/featured-image-from-url.php
@@ -4,7 +4,7 @@
  * Plugin Name: Featured Image From URL
  * Plugin URI: https://featuredimagefromurl.com/
  * Description: Use an external image as Featured Image of your post/page/custom post type (WooCommerce). Includes Auto Set (External Post), Product Gallery, Social Tags and more.
- * Version: 1.9.5
+ * Version: 1.9.6
  * Author: Marcel Jacques Machado 
  * Author URI: https://www.linkedin.com/in/marceljm/
  */
