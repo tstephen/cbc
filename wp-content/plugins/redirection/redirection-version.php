d37eb0bc8295eb7d88753a3b1c46e3e0e4d3c75b
Tim Stephenson <tim@knowprocess.com>
2019-10-23 18:14:45 +0100

    upgrade wp-content/plugins/redirection

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.3.1' );
-define( 'REDIRECTION_BUILD', '76c2e333a0fb0e417d3c1937d0c4045c' );
+define( 'REDIRECTION_VERSION', '4.4.2' );
+define( 'REDIRECTION_BUILD', 'dd4f7bc71ce67ad6763df5e0b4a4886a' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
