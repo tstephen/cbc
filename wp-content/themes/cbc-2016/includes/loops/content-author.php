22826186bc972a444a709b39383c039486137297
Tim Stephenson <tim@knowprocess.com>
2020-11-18 11:57:13 +0000

    fix to display categories and tags

--- wp-content/themes/cbc-2016/includes/loops/content-author.php
+++ wp-content/themes/cbc-2016/includes/loops/content-author.php
@@ -2,8 +2,9 @@
 	<em>
 		<span class="text-muted author">
 			<img class="img-rounded" src="<?php echo 'https://www.gravatar.com/avatar/'.md5( strtolower( get_the_author_meta('email') ) ).'?d=blank&e='.urlencode( $default ).'&s=48'; ?>">
-			<?php the_author() ?>,</span>
-		<time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
+			<?php the_author_posts_link(); ?>
+			<time class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
+		</span>
 	</em>
 </h5>
 
--- wp-content/themes/cbc-2016/includes/loops/content-single.php
+++ wp-content/themes/cbc-2016/includes/loops/content-single.php
@@ -13,8 +13,11 @@ The Single Posts Loop
               <?php get_template_part('includes/loops/content', 'author'); ?>
             <?php } ?>
             <p class="no-print text-muted" style="margin-bottom: 30px;">
-              <?php if (has_category(get_post())) { ?>
-                <i class="glyphicon glyphicon-folder-open"></i>&nbsp; <?php _e('Filed under', 'bst'); ?>: <?php the_category(', ') ?><br/>
+              <?php if (has_category()) { ?>
+                <i class="glyphicon glyphicon-folder-open"></i>&nbsp; <?php _e('Filed under', 'bst'); ?>: <?php the_category(', ') ?>&nbsp;
+              <?php } ?>
+              <?php if (has_tag()) { ?>
+                <?php _e('With themes', 'bst'); ?>: <?php echo get_the_tag_list('',', ','') ?><br/>
               <?php } ?>
               <?php if (comments_open()) { ?>
                 <i class="no-print glyphicon glyphicon-comment"></i>&nbsp; <?php _e('Comments', 'bst'); ?>: <?php comments_popup_link(__('None', 'bst'), '1', '%'); ?>
@@ -26,19 +29,18 @@ The Single Posts Loop
                 <?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive')); ?>
                 <figcaption><?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></figcaption>
             </figure>
-            <p class="print text-emphasis">
-              If you are visiting today, we&apos;re thrilled to have you with us.  We come here to worship God by singing, preaching the Bible and praying together.  God is the highest priority in our lives because through Jesus we have forgiveness from our sins and the hope of a new life spent with Him!
-            </p>
-            <ul class="print text-emphasis">
-              <li>Please switch off your mobile phone when you come in to the hall.
-              <li>Please remember to park in one of the town car parks; not along Priory St.
-              <li>Please ensure your child/ren do not play on the stage, up in the balcony or in the graveyard either before or after the service.
-              <li>We have large print bibles available for anyone who might need one. Please ask one of the ushers.
-            </ul>
+            <?php if (has_tag('notices')) { ?>
+              <p class="print text-emphasis">
+                If you are visiting today, we&apos;re thrilled to have you with us.  We come here to worship God by singing, preaching the Bible and praying together.  God is the highest priority in our lives because through Jesus we have forgiveness from our sins and the hope of a new life spent with Him!
+              </p>
+              <ul class="print text-emphasis">
+                <li>Please switch off your mobile phone when you come in to the hall.
+                <li>Please remember to park in one of the town car parks; not along Priory St.
+                <li>Please ensure your child/ren do not play on the stage, up in the balcony or in the graveyard either before or after the service.
+                <li>We have large print bibles available for anyone who might need one. Please ask one of the ushers.
+              </ul>
+            <?php } ?>
             <?php the_content()?>
-            <footer class="print text-emphasis">
-              Registered Charity no. 1148492
-            </footer>
             <?php wp_link_pages(); ?>
         </section>
     </article>
