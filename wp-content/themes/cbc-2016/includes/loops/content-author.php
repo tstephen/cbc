5f6c3c9b68ae522845a57d6519af9a5cc156e958
Tim Stephenson <tim@knowprocess.com>
2021-06-15 11:05:11 +0100

    avoid undefined var error on gravatar

--- wp-content/themes/cbc-2016/includes/loops/content-author.php
+++ wp-content/themes/cbc-2016/includes/loops/content-author.php
@@ -1,7 +1,7 @@
 <h5 class="no-print">
 	<em>
 		<span class="text-muted author">
-			<img class="img-rounded" src="<?php echo 'https://www.gravatar.com/avatar/'.md5( strtolower( get_the_author_meta('email') ) ).'?d=blank&e='.urlencode( $default ).'&s=48'; ?>">
+			<img class="img-rounded" src="<?php echo 'https://www.gravatar.com/avatar/'.md5( strtolower( get_the_author_meta('email') ) ).'?d=blank&s=48'; ?>">
 			<?php the_author_posts_link(); ?>
 			<time class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_time('jS F Y') ?></time>
 		</span>
